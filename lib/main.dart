63046047a4db49704e6a0181f927609b6a193b26
Nipun Agrawal <nipunagrawal500@gmail.com>
2025-03-21 23:07:30 +0530

    managed locations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fitness_app/firebase_options.dart';
-import 'package:fitness_app/services_firebase/login_page.dart';
+import 'package:fitness_app/services_firebase/firebase_options.dart';
+import 'package:fitness_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_app/pages/extra_widgets.dart';
-import 'package:fitness_app/services_firebase/login_page.dart';
+import 'package:fitness_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
