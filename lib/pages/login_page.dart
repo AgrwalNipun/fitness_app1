d3fe00fdd303fe5ba0079bca30302a7b98811df0
Nipun Agrawal <nipunagrawal500@gmail.com>
2025-05-04 23:31:05 +0530

    updated login screen

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -16,9 +16,11 @@ class _LoginPageState extends State<LoginPage> {
   final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
       GlobalKey<ScaffoldMessengerState>();
 
+  bool _isPasswordVisible = false;
+  FocusNode focusNode = FocusNode();
+
   Future<String> signUp(WidgetRef ref) async {
     if (emailController.text.isEmpty || passwordController.text.isEmpty) {
-      print('Enter Mail and password both');
       return "Enter Mail and password both";
     } else {
       final res = await createUserWithMailAndPassword(
@@ -36,11 +38,14 @@ class _LoginPageState extends State<LoginPage> {
       (value) => _scaffoldMessengerKey.currentState!.showSnackBar(
         SnackBar(
           behavior: SnackBarBehavior.floating,
-          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
           margin: EdgeInsets.all(10),
           backgroundColor: Colors.white,
           // padding: EdgeInsets.all(10),
-          content: Text(value,style: TextStyle(color: Colors.black),)),
+          content: Text(value, style: TextStyle(color: Colors.black)),
+        ),
       ),
     );
   }
@@ -61,38 +66,31 @@ class _LoginPageState extends State<LoginPage> {
           ),
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(flex: 2, child: SizedBox()),
-
+                SizedBox(height: 90),
                 Icon(Icons.directions_walk, size: 40, color: Colors.white),
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    children: [
-                      Text(
-                        "FITTR",
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "Get Fitter\nEveryday",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+                Text(
+                  "FITTR",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
+                SizedBox(height: 20),
+                Text(
+                  "Get Fitter\nEveryday",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20),
                 Text(
                   "Sign UP or Log In to access FITTR.",
                   style: TextStyle(color: Colors.white),
                 ),
-
+                SizedBox(height: 10),
                 Text("Enter Your Email", style: TextStyle(color: Colors.white)),
                 Container(
                   width: MediaQuery.of(context).size.width / 1.5,
@@ -122,26 +120,57 @@ class _LoginPageState extends State<LoginPage> {
                     border: Border.all(color: Colors.white),
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: TextField(
-                    showCursor: false,
-                    style: TextStyle(color: Colors.white, fontSize: 30),
-                    textAlign: TextAlign.center,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                      border: InputBorder.none,
-                    ),
-                    obscureText: true,
+                  child: Consumer(
+                    builder:
+                        (context, ref, child) => TextField(
+                          showCursor: false,
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                          textAlign: TextAlign.center,
+                          controller: passwordController,
+
+                          // readOnly: _readPassword,
+                          // focusNode: focusNode,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 10,
+                            ),
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isPasswordVisible = !_isPasswordVisible;
+                                });
+                              },
+                              icon:
+                                  _isPasswordVisible
+                                      ? Icon(
+                                        Icons.visibility,
+                                        color: Colors.white,
+                                        size: 20,
+                                      )
+                                      : Icon(
+                                        Icons.visibility_off,
+                                        color: Colors.white,
+                                        size: 20,
+                                      ),
+                            ),
+                          ),
+                          onSubmitted: (value) {
+                            focusNode.requestFocus(FocusNode());
+                            setState(() {});
+                            showSnackBarAfterSignUp(ref);
+                          },
+                          obscureText: !_isPasswordVisible,
+                        ),
                   ),
                 ),
                 SizedBox(height: 20),
-
                 Container(
                   width: MediaQuery.of(context).size.width - 100,
-
                   height: 50,
                   decoration: BoxDecoration(
-                    color: Color.fromRGBO(171, 139, 109, 1),
+                    color: Color.fromRGBO(203, 193, 184, 1),
                     borderRadius: BorderRadius.circular(20),
                   ),
                   child: Consumer(
@@ -161,12 +190,36 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 100),
+                Container(
+                  width: MediaQuery.of(context).size.width - 100,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(185, 169, 153, 1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Consumer(
+                    builder:
+                        (context, ref, child) => TextButton(
+                          onPressed: () {
+                            showSnackBarAfterSignUp(ref);
+                          },
+                          child: Center(
+                            child: Text(
+                              "Continue With Google",
+                              style: TextStyle(
+                                color: Color.fromRGBO(15, 13, 9, 1),
+                              ),
+                            ),
+                          ),
+                        ),
+                  ),
+                ),
+                SizedBox(height: 20,),
                 Text(
                   "Already Signed Up? Log In.",
                   style: TextStyle(color: Colors.white),
                 ),
-                Expanded(flex: 4, child: SizedBox()),
               ],
             ),
           ),
